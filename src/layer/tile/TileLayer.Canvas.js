d526464270b9cf82faba547fd581e45fc3205050
Morten Ditlevsen <bek@termestrup.dk>
2012-02-02 09:49:51 +0100

    Tile.Canvas now allows for loading image tiles exactly like the Tile layer. Just in case someone would like to draw the images on the canvas and save a few bytes by not having to have both img tiles and canvas tiles on the map.

--- src/layer/tile/TileLayer.Canvas.js
+++ src/layer/tile/TileLayer.Canvas.js
@@ -3,10 +3,6 @@ L.TileLayer.Canvas = L.TileLayer.extend({
 		async: false
 	},
 
-	initialize: function (options) {
-		L.Util.setOptions(this, options);
-	},
-
 	redraw: function () {
 		for (var i in this._tiles) {
 			var tile = this._tiles[i];
@@ -20,6 +16,8 @@ L.TileLayer.Canvas = L.TileLayer.extend({
 
 	_createTileProto: function () {
 		this._canvasProto = L.DomUtil.create('canvas', 'leaflet-tile');
+		this._tileImg = L.DomUtil.create('img', 'leaflet-tile');
+		this._tileImg.galleryimg = 'no';
 
 		var tileSize = this.options.tileSize;
 		this._canvasProto.width = tileSize;
@@ -32,6 +30,33 @@ L.TileLayer.Canvas = L.TileLayer.extend({
 		return tile;
 	},
 
+
+	_tileOnLoad: function (e) {
+		var layer = this._layer;
+
+		this.className += ' leaflet-tile-loaded';
+		layer.fire('tileload', {tile: this});
+
+		layer._tilesToLoad--;
+		if (!layer._tilesToLoad) {
+			layer.fire('load');
+		}
+	},
+
+	_imgOnLoad: function (e) {
+		var tile = this._tile;
+		var layer = tile._layer;
+
+		tile.className += ' leaflet-tile-loaded';
+		layer._redrawTile(tile);
+		layer.fire('tileload', {tile: this, url: this.src});
+
+		layer._tilesToLoad--;
+		if (!layer._tilesToLoad) {
+			layer.fire('load');
+		}
+	},
+
 	_loadTile: function (tile, tilePoint, zoom) {
 		tile._layer = this;
 		tile._tilePoint = tilePoint;
@@ -39,8 +64,20 @@ L.TileLayer.Canvas = L.TileLayer.extend({
 
 		this.drawTile(tile, tilePoint, zoom);
 
-		if (!this.options.async) {
-			this.tileDrawn(tile);
+		if (this._url) {
+			var url = this.getTileUrl(tilePoint, zoom);
+			var tileImg = this._tileImg.cloneNode(false);
+			tileImg._tile = tile;
+			tileImg.onselectstart = tile.onmousemove = L.Util.falseFn;
+			tileImg.onload = this._imgOnLoad;
+			tileImg.onerror = this._tileOnError;
+			tileImg.src = url;
+			tile.tileImg = tileImg;
+		}
+		else {
+			if (!this.options.async) {
+				this.tileDrawn(tile);
+			}
 		}
 	},
 
